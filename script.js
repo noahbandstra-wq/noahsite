adc8a8548f93ab671206ec9548181d78e848a6fb
noahbandstra-wq <noah.bandstra@icloud.com>
2026-02-09 21:06:27 -0600

    Enhance getName function with greeting and redirect
    Updated greeting message and added a delay before opening a YouTube link.

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 var name = "name";
 function getName() {
   name = prompt("What is your name?");
-  document.getElementById("button1").innerHTML = "hello " + name + "!";
+  document.getElementById("button1").innerHTML = "Hello " + name + "!";
+  setTimeout(() => {
+    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank', windowFeatures);
+  }, 2000);
+  
 }
 document.getElementById("button1").addEventListener("click", getName);
